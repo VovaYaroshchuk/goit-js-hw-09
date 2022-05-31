0787eb0ad61035419a0e8a2c434ebbe278d1cb79
Vova Yaroshchuk <voyaroshchuk@gmail.com>
2022-05-31 15:55:06 +0300

    final fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+import throttle  from "lodash.throttle";
+const player = new Player('vimeo-player');
 
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))

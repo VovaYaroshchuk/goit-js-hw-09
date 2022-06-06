1275e98b95a1d6af032e74e1d2f0233fb32fcb43
VovaYaroshchuk <95219124+VovaYaroshchuk@users.noreply.github.com>
2022-06-06 19:56:22 +0000

    Deploying to gh-pages from @ VovaYaroshchuk/goit-js-hw-09@96c719d310eb3a89af0e867e314499885572992f 🚀

--- 01-color-switcher.88f81882.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){colorSwitcher=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(colorSwitcher),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.88f81882.js.map
